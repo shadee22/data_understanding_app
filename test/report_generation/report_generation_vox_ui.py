35efbe849fb915fe4ba59c69b6ec2b47467fe2c2
shadee22 <shadeersadikeen@gmail.com>
2024-07-30 09:29:26 +0530

    test: succuss of working 0.2 v

--- test/report_generation/report_generation_vox_ui.py
+++ test/report_generation/report_generation_vox_ui.py
@@ -10,13 +10,9 @@ import os
 import plotly.express as px
 
 def get_file_created_time(uploaded_file):
-    # Save the uploaded file temporarily to access its metadata
     with open(uploaded_file.name, "wb") as f:
         f.write(uploaded_file.getbuffer())
-    
-    # Get the absolute path of the saved file
     file_path = os.path.abspath(uploaded_file.name)
-    
     created_time = os.path.getctime(file_path)
     created_time_dt = datetime.utcfromtimestamp(created_time)
     sri_lanka_tz = pytz.timezone('Asia/Colombo')
@@ -24,11 +20,11 @@ def get_file_created_time(uploaded_file):
     created_time_utc = pytz.utc.localize(created_time_dt)
     created_time_sl = created_time_utc.astimezone(sri_lanka_tz)
     created_time_jp = created_time_sl.astimezone(japan_tz)
-    
-    # Clean up the temporary file
     os.remove(file_path)
-    
     return created_time_jp
+
+def generate_header_text(base_text, session_key):
+    return f"**{base_text} (Generated at: {st.session_state[session_key]} JST)**" if session_key in st.session_state and st.session_state[session_key] else base_text
 
 # Initialize session state variables if they don't exist
 if 'generated_cardholder' not in st.session_state:
@@ -40,28 +36,18 @@ if 'generated_redemptions' not in st.session_state:
 
 tab1, tab2 = st.sidebar.tabs(["Upload Data Files", "Generated Time"])
 
-# First tab for uploading data files
 with tab1:
     st.header('Upload Data Files')
     card_data = st.file_uploader("Upload Card Data", type=['csv'])
     transaction_data = st.file_uploader("Upload Transaction Data", type=['csv'])
     redemption_data = st.file_uploader("Upload Redemption Data", type=['csv'])
 
-
-def update_ui(input_text):
-    st.write(f"You typed: {input_text}")
-    
-    
 with tab2:
     st.header('Data Source Generated Time ')
-
-    # Update session state directly for real-time effect
     st.session_state['generated_cardholder'] = st.text_input("Generated Cardholder Time", value=st.session_state['generated_cardholder'])
     st.session_state['generated_transactions'] = st.text_input("Generated Transactions Time", value=st.session_state['generated_transactions'])
     st.session_state['generated_redemptions'] = st.text_input("Generated Redemptions Time", value=st.session_state['generated_redemptions'])
-
     st.markdown('[Time Converter](https://savvytime.com/converter/sri-lanka-colombo-to-japan-ueno-ebisumachi)')
-
 
 if card_data and transaction_data and redemption_data:
     card_df = load_card_data(card_data)
@@ -72,74 +58,51 @@ if card_data and transaction_data and redemption_data:
     num_unique_cards = get_unique_cards(card_df)
     value_counts = cardholder_card_count(card_df)
 
-    # Use the uploaded card data file to get the creation time
     a_time = get_file_created_time(card_data)
     formatted_time = a_time.strftime("%I:%M %p %Z")
-    
-    # Get the current date
     current_date = datetime.now().strftime("%B %d, %Y")
 
-    st.image('./other/assets/banner+pulse+id.png')  
-    
+    st.image('./other/assets/banner+pulse+id.png')
     st.title(f"VISA Japan Campaign Analysis {current_date}")
-    
-    header_text = f"**Card and Cardholder Information Analysis (Generated at: {st.session_state['generated_cardholder']} JST)**" if 'generated_cardholder' in st.session_state and st.session_state['generated_cardholder'] else "Card and Cardholder Information Analysis"
-    st.header(header_text)
-        
-        
+
+    st.header(generate_header_text("Card and Cardholder Information Analysis", 'generated_cardholder'))
     st.write(f"Number of unique Cardholders: **{num_unique_cardholders}**")
     st.write(f"Number of unique Cards: **{num_unique_cards}**")
-
     st.header("Card Count Analysis (Cards per Cardholder)")
     st.dataframe(value_counts, use_container_width=True)
-    
-    # SPACING
     st.write("#")
     st.write("#")
-    
     fig2 = px.pie(value_counts, values="unique_cardholder_count", names="card_count", title="Cardholder Distribution by Card Count", hole=0.5)
     st.plotly_chart(fig2)
     st.plotly_chart(daily_cardholder_enrollment(card_df))
-
-    # SPACING
     st.write("#")
 
-    st.header(f"Top Issuer Analysis")
+    st.header("Top Issuer Analysis")
     grouped_df = top_issuers.top_issuer_analysis(card_df=card_df)
     st.dataframe(grouped_df, use_container_width=True)
     fig3 = px.bar(grouped_df, x="card_id_count", y="Bank Name", orientation='h', title="Card ID Counts by Top Issuers")
     st.plotly_chart(fig3, use_container_width=True)
 
-    # SPACING
+    
+    st.write("#")
     st.write("#")
 
-    header_text = f"**Transaction Analysis (Generated at: {st.session_state['generated_transactions']} JST)**" if 'generated_transactions' in st.session_state and st.session_state['generated_transactions'] else "Transaction Analysis"
-    st.header(header_text)
-
-    # st.header(f"Transaction Analysis (Generated at: {st.session_state['generated_transactions']} JST)")
+    
+    
+    st.header(generate_header_text("Transaction Analysis", 'generated_transactions'))
     transaction_metrics_df = transaction_metrics(transaction_df)
     st.dataframe(transaction_metrics_df, use_container_width=True)
-
-    # SPACING
     st.write("#")
 
-    header_text = f"**Redemption Analysis (Generated at: {st.session_state['generated_redemptions']} JST)**" if 'generated_redemptions' in st.session_state and st.session_state['generated_redemptions'] else "Redemption Analysis"
-    st.header(header_text)
-
-    # st.header(f"Redemption Analysis (Generated at: {st.session_state['generated_redemptions']} JST)")
+    st.header(generate_header_text("Redemption Analysis", 'generated_redemptions'))
     redemption_metrics_df = redemption_metrics(redemption_df)
     st.dataframe(redemption_metrics_df, use_container_width=True)
     st.plotly_chart(daily_redemptions_value(redemption_df))
     st.plotly_chart(daily_redemptions_count(redemption_df))
 
-
-    header_text = f"**Merchant-wise Total Redemption Analysis (Generated at: {st.session_state['generated_redemptions']} JST)**" if 'generated_redemptions' in st.session_state and st.session_state['generated_redemptions'] else "Merchant-wise Total Redemption"
-    st.header(header_text)
-    # st.header(f"Merchant-wise Total Redemption Analysis (Generated at: {st.session_state['generated_redemptions']})")
+    st.header(generate_header_text("Merchant-wise Total Redemption Analysis", 'generated_redemptions'))
     merchant_redemptions_df = merchant_wise_redemption(redemption_df)
     st.dataframe(merchant_redemptions_df, use_container_width=True)
-    
-    # SPACING
     st.write("#")
     st.write("#")
     st.write("#")
